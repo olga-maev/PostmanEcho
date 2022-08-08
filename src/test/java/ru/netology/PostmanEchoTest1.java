aa355e034313cc2b39cf1f024f445f0321dadaeb
olga-maev <kurumoch0@gmail.com>
2022-08-08 14:38:00 +0400

    Test autorization off

--- src/test/java/ru/netology/PostmanEchoTest1.java
+++ src/test/java/ru/netology/PostmanEchoTest1.java
@@ -3,16 +3,15 @@ package ru.netology;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
-public class PostmanEchoTest {
+public class PostmanEchoTest1 {
     @Test
-    void souldPostmanEchoPost() {
+    void souldPostmanEchoPostAutorization() {
         // Given - When - Then
 // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .headers("Authorization","Basic cG9zdG1hbjpwYXNzd29yZA==")
+                .headers("Authorization","cG9zdG1hbjpwYXNzd29yZA==")
 //   Выполняемые действия
                 .when()
                 .get("/basic-auth")
